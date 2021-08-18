60360d0cc3f0403a35c6f6f61ada3ac8942e5c87
OMtsk <e8719@g.maizuru-ct.ac.jp>
2021-08-18 19:00:41 +0900

    prepare to add room

--- server/ws_server.js
+++ server/ws_server.js
@@ -1,5 +1,5 @@
 'use strict';
-//TODO: room
+
 /*この定義方法気持ち悪い（個人的に）*/
 exports.startSocketServer = function(httpServer){
     const io = require('socket.io')(httpServer, {serverClient: true});
@@ -7,37 +7,67 @@ exports.startSocketServer = function(httpServer){
     /* Serverはconnectじゃなくてconnectionらしい*/
     io.on("connection", socket => {
         doConnect(socket);
-        console.log("GenUUID: " + socket.id);
+
+        socket.on("changeUUID", uuid => {
+            changeUUID(socket, uuid);
+        });
+
+        socket.on("createRoom", arg => {
+            createRoom(socket, arg);
+        });
+        
+        socket.on("joinRoom", arg => {
+            joinRoom(socket, arg);
+        });
+
+
 
         socket.on("message", arg => {
             doMessage(socket, arg);
         });
+
     });
 }
 
+
 //TODO: 関数名変える(doはないぞー)
 function doConnect(socket) {
-    console.log("Client connect")
-    socket.id = generateUuid();
-    socket.emit('updateUUID', {id: socket.id});
+    console.log("Client connect: " + socket.id)
+ //   socket.emit('updateUUID', {id: socket.id});
 }
+
+function changeUUID(socket, uuidd) {
+    console.log("UPDATE UUID: " + uuid);
+    socket.id = uuid;
+}
+
+function createRoom(socket, arg) {}
+function joinRoom(socket, arg) {}
 
 function doMessage(socket, message){
-        try{
-            var json = JSON.parse(message.toString());
-
-            if(json.task.broadcast == null)
-                return;
-
-            json.task.broadcast.forEach(event => {
-                socket.broadcast.emit('broadcast', JSON.stringify(event));
-            });
-        }catch (e) {
-            console.log('invalid json: ' + message);
-            return;
-        }
+    console.log("Catch message: " + message);
+    broadcastMessage(socket, message);   
 }
 
+function broadcastMessage(socket, message) {
+    try{
+        var json = JSON.parse(message.toString());
+        console.log(json);
+
+        if(json.task.broadcast == null)
+            return;
+
+        json.task.broadcast.forEach(event => {
+            socket.broadcast.emit('broadcast', JSON.stringify(event));
+        });
+
+    }catch (e) {
+        console.log('invalid json: ' + message);
+        return;
+    }
+}
+
+//TODO: Delete below function
 function generateUuid() {
     // https://github.com/GoogleChrome/chrome-platform-analytics/blob/master/src/internal/identifier.js
     // const FORMAT: string = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
