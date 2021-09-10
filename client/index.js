cd87ac9fecc2c55246a84d5ddb5a02809a062a1e
u-haru <40634644+u-haru@users.noreply.github.com>
2021-09-11 02:42:38 +0900

    自由に動かせるウィンドウを追加

--- client/index.js
+++ client/index.js
@@ -20,8 +20,6 @@ window.addEventListener("load", () => {
     
     document.getElementById("join_room").onclick = () => {room_button("join")}
     document.getElementById("create_room").onclick = () => {room_button("create")}
-    document.getElementById("gamestart_button").onclick = () => {game_button("start")}
-    document.getElementById("gamestop_button").onclick = () => {game_button("stop")}
     let overlays = Array.from( document.getElementsByClassName('overlay') ) ;
     overlays.forEach(element => {
         for (const eventName of ['mouseup','mousedown', 'touchstart', 'touchmove', 'touchend', 'touchcancel']){
@@ -122,4 +120,94 @@ function createElementFromHTML(html){
     html = html.trim(); // Never return a text node of whitespace as the result
     template.innerHTML = html;
     return template.content.firstElementChild;
+}
+
+
+// 例
+// window["CreateDraggableWindowFromHTML"](`<div style="display: flex;align-items: center;justify-content: space-evenly;">
+//              <input value="ゲーム開始" type="button" id="gamestart_button"></input>
+//              <input value="ゲーム終了" type="button" id="gamestop_button"></input>
+//              </div>`,
+//     "title",{
+//     xbutton: true
+//   })
+var windowindex = 0;//ウィンドウをドラッグするたびに増やす
+function CreateDraggableWindowFromHTML(html,title,configs){
+    let windowhtml = createElementFromHTML(`
+    <div class="window" style="${(configs != null && configs.style)?configs.style:""}">
+        <div class="window-titlebar">
+            <span>${title}</span>
+            ${(configs != null && configs.xbutton)?'<span class="Xbutton" onclick="this.parentElement.parentElement.remove()"></span>':''}
+        </div>
+        ${html}
+    </div>`)
+    let x;
+    let y;
+    const windows = document.getElementById("htmlwindows")
+    let drag = windows.insertAdjacentElement('afterbegin',windowhtml)
+
+    drag.style.top = Math.random()*(document.documentElement.clientHeight/2 - drag.clientHeight) + "px"
+    drag.style.left = Math.random()*(document.documentElement.clientWidth/2 - drag.clientWidth) + "px"
+    
+    //イベントを吸収
+    for (const eventName of ['mouseup','mousedown', 'touchstart', 'touchmove', 'touchend', 'touchcancel']){
+        drag.addEventListener(eventName, e => e.stopPropagation(),{passive: true});
+    }
+
+    for (const eventName of ['mousedown', 'touchstart']){
+        const clickwindow = (e) => {
+            drag.style["z-index"] = ++windowindex;
+        }
+        drag.addEventListener(eventName,clickwindow);
+    }
+    //ウィンドウ内をクリックしたら発火
+    const clickwindow = (e) => {
+        drag.style["z-index"] = ++windowindex;
+    }
+    const mdown = (e) => {
+        drag.classList.add("dragging");
+        let event = e;
+        //タッチデイベントとマウスのイベントの差異を吸収
+        if(!(e.type === "mousedown")) {
+            event = e.changedTouches[0];
+        }
+        //要素内の相対座標
+        x = event.pageX - drag.offsetLeft;
+        y = event.pageY - drag.offsetTop;
+        //ムーブイベントにコールバック
+        document.body.addEventListener("mousemove", mmove, false);
+        document.body.addEventListener("touchmove", mmove, false);
+        
+        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
+        drag.addEventListener("mouseup", mup, false);
+        drag.addEventListener("touchend", mup, false);
+        document.body.addEventListener("mouseleave", mup, false);
+        document.body.addEventListener("touchleave", mup, false);
+    }
+    //マウスカーソルが動いたときに発火
+    const mmove = (e) => {
+        let event = e;
+        if(!(e.type === "mousemove")) {
+            event = e.changedTouches[0];
+        }
+        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+        e.preventDefault();
+
+        drag.style.top = event.pageY - y + "px";
+        drag.style.left = event.pageX - x + "px";
+    }
+    //マウスボタンが上がったら発火
+    const mup = (e) => {
+        //ムーブベントハンドラの消去
+        drag.removeEventListener("mouseup", mup, false);
+        drag.removeEventListener("touchend", mup, false);
+        document.body.removeEventListener("mousemove", mmove, false);
+        document.body.removeEventListener("touchmove", mmove, false);
+        //クラス名 .drag も消す
+        drag.classList.remove("dragging");
+    }
+    
+    //ウィンドウバークリックで発火
+    drag.firstElementChild.addEventListener("mousedown", mdown, false);
+    drag.firstElementChild.addEventListener("touchstart", mdown, false);
 }
