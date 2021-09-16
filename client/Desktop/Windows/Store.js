df82f8b563803cfd0a47ebf7f7e126c30e05bfd5
ryoya <e8758@g.maizuru-ct.ac.jp>
2021-09-16 13:30:16 +0900

    ストアウィンドウの作成

--- client/Desktop/Windows/Store.js
+++ client/Desktop/Windows/Store.js
@@ -1,6 +1,7 @@
 'use strict'
 import { Window } from "../Window.js"
 import { SystemConfigs } from "../Desktop.js"
+import { Desktop } from "../../scene/Desktop.js"
 
 const html = `
 <div style="display: flex;flex-direction: column;justify-content: space-between;height: 100%;">
@@ -25,38 +26,150 @@ const html = `
         align-items: center;
         border: 1px solid #cccccc;
     }
-    .list_container.is-hidden{
-        visibility: hidden;
+    .is_hidden{
+        display:none;
     }
     .store_container{
-        display: inline-block;
-        width: 100%;
-        height: 100%;
-        margin:0em 0em ;
-        box-sizing: border-box;
-        background-color: #f0f0f0;
-        padding: 2px;
-        user-select: none;
-        align-items: center;
+        display:flex;
+        width:750px;
+        height:250px;
+        margin:3em 3.5em;
+    }
+    .store_background{
+        width:750px;
+        height:250px;
+        box-sizing:border-box;
+        background-color:#ffffff;
         border: 1px solid #cccccc;
+        user-select: none;
     }
     .app_icon{
         position:relative;
         top:5px;
         left:17px;
     }
+    .app_icon2{
+        position:absolute;
+        left:120px;
+        top:100px;
+    }
+    .title2{
+        position:absolute;
+        font-size:2em;
+        top:80px;
+        left:325px;
+    }
+    .copname{
+        position:absolute;
+        font-size:1em;
+        color: #0000FF;
+        top:115px;
+        left:325px;
+        white-space: nowrap;
+    }
+    .corp_shortdescription{
+        white-space: pre;
+        position:absolute;
+        font-size:1.2em;
+        top:150px;
+        left:330px;
+    }
+    .install_btn{
+        white-space: pre;
+        position:absolute;
+        height:40px;
+        left: 325px;
+        top: 245px;
+        box-sizing: border-box;
+        background-color: #33cccc;
+        margin:0em 0em;
+        padding: 10px;
+    }
+    .price{
+        white-space: pre;
+        position:absolute;
+        font-size:1.2em;
+        top:220px;
+        left:340px;
+    }
+    .detail{
+        white-space: pre;
+        position:absolute;
+        font-size:1.2em;
+        top:300px;
+        left:400px;
+    }
+    .platform_subject{
+        white-space: pre;
+        position:absolute;
+        font-size:1.6em;
+        top:325px;
+        left:100px;
+    }
+    .platform_text{
+        white-space: pre;
+        position:absolute;
+        font-size:1.2em;
+        top:370px;
+        left:200px;
+    }
+    .author_subject{
+        white-space: pre;
+        position:absolute;
+        font-size:1.6em;
+        top:420px;
+        left:100px;
+    }
+    .author_text{
+        white-space: pre;
+        position:absolute;
+        font-size:1.2em;
+        color: #0000FF;
+        top:460px;
+        left:100px;
+    }
+    .function_subject{
+        white-space: pre;
+        position:absolute;
+        font-size:1.6em;
+        top:325px;
+        left:500px;
+    }
+    .function_text{
+        white-space: pre;
+        position:absolute;
+        font-size:1.2em;
+        top:370px;
+        left:480px;
+    }
+    .backbtn{
+        position:absolute;
+        top:0px;
+        left:0px;
+        width: 57px;
+        height: 50px;
+        line-height: 50px;
+        background-color: #808080;
+        color: #fff;
+    }
     .star_icon{
         position:relative;
         top:-20px;
         left:10px;
     }
+    .star_icon2{
+        position:absolute;
+        top:190px;
+        left:550px;
+        font-size: 2em;
+    }
     .star5_rating{
-    position: relative;
-    z-index: 0;
-    display: inline-block;
-    white-space: nowrap;
-    color: #CCCCCC; /* グレーカラー 自由に設定化 */
-    /*font-size: 25px;  フォントサイズ 自由に設定化 */
+        position: relative;
+        z-index: 0;
+        display: inline-block;
+        white-space: nowrap;
+        color: #CCCCCC; /* グレーカラー 自由に設定化 */
+        /*font-size: 25px;  フォントサイズ 自由に設定化 */
     }
 
     .star5_rating:before, .star5_rating:after{
@@ -87,8 +200,8 @@ export class Store extends Window {
         super(html, "Store", parent, { style: style });
         this.apps = this.get_apps()
         this.list_container = this.bodyElem.firstElementChild.firstElementChild
-        this.store_container = this.bodyElem.firstElementChild.firstElementChild
         let allapp = ''
+        this.selectapp = ""     //　選んだアプリを記憶
 
         allapp += `<div class = "title">
         おすすめアプリ
@@ -113,29 +226,102 @@ export class Store extends Window {
             </div>`
             allapp += container
         })
-        this.list_container.insertAdjacentHTML('beforeend', allapp)
+        let container2 = `
+            <div class="store_container">
+                <div class = "store_background">  </div>
+                <div class = "app_icon2">
+                    <img src = '../images/apps/AntiVirus.png' width="140px" height="140px">
+                </div>
 
-        /*
-        let container2 =
-            `<div class = "store_container">
-                <div class = "app_icon">
-                    <img src = '../images/apps/AntiVirus.png' width="110px" height="110px">
-                <div>
+                <div class = "title2" id = "title2">AntiVirus</div>
+                <div class = "copname" id = "corpname">©Anti Virus Corporation</div>
+                <div class = "corp_shortdescription" id = "corp_shortdescription">
+                ウイルス対策ソフト
+                誰にとっても使いやすくて信頼度も高い</div>
+
+                <div class = "install_btn" id = "install_btn">インストール
+                </div>
+
+                <div class = "price" id = "price">無料
+                </div>
+
+                <div class = "detail">概要</div>
+
+                <div class = "platform_subject">対応プラットフォーム</div>
+                <div class = "platform_text" id = "platform">PC</div>
+
+                <div class = "author_subject">公開元・著作権</div>
+                <div class = "author_text" id = "author">©Anti Virus Corporation</div>
+
+                <div class = "function_subject">機能</div>
+                <div class = "function_text" id = "function">
+                ・ウイルスの定期スキャン
+                ・ウイルス検出時の除去
+                ・不審なソフトウェアのブロック</div>
+
+                <div class = "backbtn">back</div>
+
             </div>`
-        */
+        allapp += container2
+        this.list_container.insertAdjacentHTML('beforeend', allapp)
+        for (let j = 0; j < this.list_container.children[this.list_container.childElementCount - 1].childElementCount; j++) {
+            this.list_container.children[this.list_container.childElementCount - 1].children[j].classList.toggle('is_hidden')
+        }
+        this.list_container.children[this.list_container.childElementCount - 1].classList.toggle('is_hidden')
 
-        for (let i = 0; i < this.list_container.childElementCount; i++) {
+        for (let i = 0; i < this.list_container.childElementCount - 1; i++) {
             this.list_container.children[i].addEventListener('click', () => {
-                this.list_container.classList.toggle('is-hidden')
+                // 表示するオブジェクトを入れ替える
+                for (let j = 0; j < this.list_container.childElementCount; j++) {
+                    this.list_container.children[j].classList.toggle('is_hidden')
+                }
+                for (let j = 0; j < this.list_container.children[this.list_container.childElementCount - 1].childElementCount; j++) {
+                    this.list_container.children[this.list_container.childElementCount - 1].children[j].classList.toggle('is_hidden')
+                }
+                this.list_container.children[this.list_container.childElementCount - 1].classList.toggle('is_hidden')
+
+                // 選んだアプリを記憶
+                this.selectapp = this.fix_text(this.apps[i - 1][0])
+
+                // テキスト変更
+                this.change_text(this.apps[i - 1])
+
             })
         }
-        this.connecttxt = this.bodyElem.firstElementChild.firstElementChild.nextElementSibling.firstElementChild
-        /**@type {any} */
-        this.connectbtn = this.bodyElem.firstElementChild.firstElementChild.nextElementSibling.lastElementChild
-        this.connectbtn.addEventListener('click', () => {
+        // backボタンが押された時
+        this.list_container.children[this.list_container.childElementCount - 1].children[this.list_container.children[this.list_container.childElementCount - 1].childElementCount - 1].addEventListener('click', () => {
+            // 表示するオブジェクトを入れ替える
+            for (let j = 0; j < this.list_container.childElementCount; j++) {
+                this.list_container.children[j].classList.toggle('is_hidden')
+            }
+            for (let j = 0; j < this.list_container.children[this.list_container.childElementCount - 1].childElementCount; j++) {
+                this.list_container.children[this.list_container.childElementCount - 1].children[j].classList.toggle('is_hidden')
+            }
+            this.list_container.children[this.list_container.childElementCount - 1].classList.toggle('is_hidden')
 
+            // 記憶したアプリをリセット
+            this.selectapp = ""
         })
 
+        // installボタンが押された時
+        this.list_container.children[this.list_container.childElementCount - 1].children[5].addEventListener('click', () => {
+            //  インストール済みなら
+            if (SystemConfigs.installed_software.includes(this.selectapp)) {
+                document.getElementById("install_btn").innerText = "インストール"
+
+                //　保存されている配列の位置を検索
+                var loc = SystemConfigs.installed_software.indexOf(this.selectapp)
+
+                //　Configのinstalled_softwareからアプリを削除
+                SystemConfigs.installed_software.splice(loc, 1)
+            }
+            else {　// インストールしていなければ
+                document.getElementById("install_btn").innerText = "アンインストール"
+
+                //  Configのinstalled_softwareにアプリを追加
+                SystemConfigs.installed_software.push(this.selectapp)
+            }
+        })
 
         // document.getElementById("gamestart_button").onclick = () => {this.game_button("start")};
         // this.bodyElem.firstElementChild.nextElementSibling.firstChild.addEventListener('click',() => {this.game_button("start")})
@@ -164,6 +350,24 @@ export class Store extends Window {
         text = text.replaceAll("\n", " ")
         text = text.substr(0, 12)
         return text
+    }
+    fix_text(text) {
+        return text
+    }
+    change_text(app) {
+        document.getElementById("title2").innerText = app[0]
+        document.getElementById("corpname").innerText = app[5]
+        document.getElementById("corp_shortdescription").innerText = app[3]
+        document.getElementById("author").innerText = app[5]
+        document.getElementById("function").innerText = app[4]
+        document.getElementById("price").innerText = app[2]
+
+        if (SystemConfigs.installed_software.includes(this.selectapp)) {
+            document.getElementById("install_btn").innerText = "アンインストール"
+        }
+        else {　// インストールしていなければ
+            document.getElementById("install_btn").innerText = "インストール"
+        }
     }
 }
 
