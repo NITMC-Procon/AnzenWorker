8e5655b5532ed11c827787bab95b91dd74d9e0f8
ryoya <e8758@g.maizuru-ct.ac.jp>
2021-09-26 15:37:28 +0900

    怪しいソフトを導入したときの機能追加

--- client/Desktop/Windows/VirusScanner.js
+++ client/Desktop/Windows/VirusScanner.js
@@ -1,5 +1,6 @@
 'use strict'
 import { Window } from "../Window.js"
+import { SystemConfigs } from "../Desktop.js"
 
 const html = `
 <div style="display: block;width: 100%;height: 100%;user-select:none;background-color:#3399cc">
@@ -14,6 +15,10 @@ const html = `
       <ul>
         <li>ウイルススキャナーはあなたのPCを監視しています。</li>
       </ul>
+      <div>
+        <div class = "bad">×</div>
+        <div class = "bad2">お使いのPCは危険な状態にさらされています。</div>
+      </div>
       <div class = "block">
         <ul2>
           <li2>リアルタイム保護:  ON</li2>
@@ -92,6 +97,27 @@ const html = `
     border-bottom: 20px solid #25AF01;
     transform: rotate(-45deg);
   }
+  ul3 {
+  list-style-type: none;
+  }
+  li3 {
+    position: relative;
+    font-size:1em;
+    text-align: center;
+    padding-top:15px;
+  }
+  li3::after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0em;
+    left: 0em;
+    width: 35px;
+    height: 20px;
+    border-left: 20px solid #ff0000;
+    border-bottom: 20px solid #ff0000;
+    transform: rotate(-45deg);
+  }
   ul2 {
   list-style-type: none;
   }
@@ -116,12 +142,47 @@ const html = `
     margin-top:50px;
     background-color:c0c0c0;
   }
+  .bad{
+    display:inline-block;
+    text-align:left;
+    color: #ff0000;
+    font-size:4em;
+    width:60px;
+  }
+  .bad2{
+    display:inline;
+    width:200px;
+  }
 </style>`
 const style = "width:40em;height:27em;"
 
 export class VirusScanner extends Window {
   constructor(parent) {
     super(html, "VirusScanner", parent, { style: style });
+
+    /** @type {HTMLElement} *///@ts-ignore
+    this.condition_var = this.bodyElem.firstElementChild.firstElementChild.firstElementChild
+    /** @type {HTMLElement} *///@ts-ignore
+    this.item2 = this.bodyElem.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling
+    /** @type {HTMLElement} *///@ts-ignore
+    this.supervise_check = this.item2.firstElementChild.firstElementChild
+    /** @type {HTMLElement} *///@ts-ignore
+    this.bad1 = this.item2.firstElementChild.firstElementChild.nextElementSibling
+
+    this.bad1.hidden = true
+
+    for (var i = 0; i < SystemConfigs.installed_software.length; i++) {
+      if (SystemConfigs.installed_software[i][1] == "danger") {
+        this.inform_danger(SystemConfigs.installed_software[i])
+      }
+    }
+  }
+  // 不正ソフト検出 or 怪しいウイルス対策ソフト導入
+  inform_danger(dangerous_software) {
+    this.condition_var.style.backgroundColor = "#ff0000"
+    this.condition_var.innerText = "コンピュータステータス - 危険な状態にあります"
+    this.supervise_check.hidden = true
+    this.bad1.hidden = false
 
   }
 }
