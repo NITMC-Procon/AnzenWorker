9939601e2c1e2e4d79779121c39c82822433c34f
Haruroid <Haruroid@users.noreply.github.com>
2021-10-09 20:29:54 +0900

    Browser修正

--- client/Desktop/Windows/Browser.js
+++ client/Desktop/Windows/Browser.js
@@ -1,5 +1,6 @@
 'use strict'
-import { SystemConfigs } from "../Desktop.js";
+import { AdVirusService1 } from "../../Services/Service/AdVirusService1.js";
+import { CallService, SystemConfigs } from "../Desktop.js";
 import { Window } from "../Window.js"
 import { Installer } from "./Installer.js";
 
@@ -177,7 +178,7 @@ function phishing1() {
 }
 
 function porn1() {
-    let ihtml = `<div style="width: 100%;height: 100%;background-color: black;color: pink;">
+    let ihtml = `<div style="width: 100%;height: 60em;background-color: black;color: pink;">
     <h1>過激無料動画</h1>
     <img src="/images/virus/mosaic_porn.png" width="100%">
     <h2>※お楽しみいただくには専用プレイヤーをダウンロードしてください</h2>
@@ -189,7 +190,7 @@ function porn1() {
 
     document.getElementById('dlplayer').onclick = onClickDownload;
     function onClickDownload() {
-        createdownload({ Name: "player.exe", Clickfunc: () => { new Installer() } })
+        createdownload({ Name: "player.exe", Clickfunc: () => { CallService(AdVirusService1) } })
     }
     
 }
