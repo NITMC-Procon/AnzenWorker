34d4e69a8de424383ef2c5bc99987668fd7f7d8e
ryoya <e8758@g.maizuru-ct.ac.jp>
2022-03-05 17:16:02 +0900

    動画（スライド）コンテンツの作成

--- client/Windows/Window/YoTube.js
+++ client/Windows/Window/YoTube.js
@@ -88,7 +88,17 @@ const html = `
       <div style = "display:flex; height:100%; width:100%;">
         <div style = "width:70%; height:100%;">
           <div style = "width:96%; height:68%; margin-left:2%; margin-top:2%; background:black;">
-
+            <div style = "width:100%; height:90%;">
+            
+            </div>
+            <div style = "display:flex; width:100%; height:10%; background:gray;">
+              <div style = "width:10%; height:10%; color:white; margin-left:10%;">
+                <b>←</b>
+              </div>
+              <div style = "width:10%; height:10%; color:white;">
+                <b>→</b>
+              </div>
+            </div>
           </div>
           <div style = "width:90%; height:28%; margin-left:5%; margin-top:2%;">
             <div style = "width:100%; height:50%; border-bottom:1px solid gray;">
@@ -157,7 +167,115 @@ export let contents = [
   {
     title: "【必見！】安全なWi-Fiの見分け方を解説",
     thumb: "安全なWi-Fiの見分け方",
-    disc: "678回視聴・1日前"
+    disc: "678回視聴・1日前",
+    pages: `
+    <div id = "p1" style = "width:100%; height:100%; background:white;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・Wi-Fi（ワイファイ）って何？
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        ・携帯などを無線でネットワークに接続する技術のこと
+      </div>
+    </div>
+    <div id = "p2" style = "width:100%; height:100%;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・Wi-Fiの何が危険なの？
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        ・Wi-Fiの電波は誰でも盗聴できるところ
+      </div>
+    </div>
+    <div id = "p3" style = "width:100%; height:100%;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・Wi-Fiにつないではいけないの？
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        ・「暗号化」されているWi-Fiなら"まだ"安全
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        ・フリーWi-Fiに「暗号化」されていないものがよく混じっている
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        ・だからフリーWi-Fiは注意が必要
+      </div>
+    </div>
+    <div id = "p4" style = "width:100%; height:100%;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・暗号化されているかってどう見分けるの？
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        <img src = "../images/padlock.png" style = " height:80%; width:auto; margin-top:5%; margin-left:5%;">
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        ・Wi-Fiの名前の横に、上の写真のような鍵があれば「暗号化」されているサイン
+      </div>
+    </div>
+    <div id = "p5" style = "width:100%; height:100%;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・暗号化されていれば何でもいいの？
+      </div>
+      <div style = "width:92%; height:20%; margin-left:8%;">
+        ・暗号化にも種類があり、より解読されにくい方法で暗号化されたものを選ぼう
+      </div>
+    </div>
+    <div id = "p6" style = "width:100%; height:100%;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・安全性が高い順
+      </div>
+      <div style = "width:92%; height:10%; margin-left:8%;">
+        ・セキュリティ規格
+      </div>
+      <div style = "width:92%; height:10%; margin-left:8%;">
+        弱 WEP < WPA < WPA2 強
+      </div>
+      <div style = "width:92%; height:10%; margin-left:8%;">
+        ・暗号化方式
+      </div>
+      <div style = "width:92%; height:10%; margin-left:8%;">
+        弱 TKIP < AES 強
+      </div>
+    </div>
+    <div id = "p7" style = "width:100%; height:100%;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・さらに、
+      </div>
+      <div style = "width:92%; height:10%; margin-left:8%;">
+        企業向けに、WPA/WPA2-Enterprise(IEEE 802.1X認証)というさらに安全なものがある
+      </div>
+    </div>
+    <div id = "p8" style = "width:100%; height:100%;">
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.2em;">
+        安全なWi-Fiの選び方
+      </div>
+      <div style = "width:95%; height:20%; margin-left:5%; font-size:1.1em;">
+        ・メッセージ
+      </div>
+      <div style = "width:92%; height:10%; margin-left:8%;">
+        難しい人は、名前だけでも覚えてより安全なWi-Fiに接続できるように頑張ろう！
+      </div>
+    </div>
+    `
   }, {
     title: "ストアのアプリなら安全だと思っていませんか？",
     thumb: "「ストアのアプリ」実は....",
